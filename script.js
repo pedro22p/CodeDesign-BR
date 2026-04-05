07088ef02f46aa4dd6ce1ddeda48a4ef1d8e2de5
pedro <pedrorafaelpadilhasartori22@gmail.com>
2026-04-05 18:34:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnWhats = document.getElementById('btn-whats');
+
+btnWhats.addEventListener('click', function() {
+    console.log("O usuário clicou no link do WhatsApp!");
+    // Você pode adicionar um alerta se quiser:
+    // alert("Redirecionando para o WhatsApp...");
+});
